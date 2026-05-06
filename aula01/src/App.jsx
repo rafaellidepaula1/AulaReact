48961aa9f4ae93789a4008a6d68d524bcfa40517
rafaellidepaula1 <rafaellidepaula1@gmail.com>
2026-05-06 14:20:44 -0300

    06/05

--- aula01/src/App.jsx
+++ aula01/src/App.jsx
@@ -1,18 +1,8 @@
-import "./app.css";
-
 export default function app()
-  {
-   let nome, idade, dias
-   nome= "rafaelli";
-   idade= 16;
-   dias= idade * 365;
-  return(
+{
+  return (
     <div>
-      <h1>Ola mundo !!!</h1>
-      <div className="conteudo">
-        <p>Olá seja bem vindo ao react com js.</p>
-        <p> O aluno {nome} ja viveu {dias} dias.</p>
-      </div>
+      <h1>Aula 02 - estudo de componentes de props</h1>
     </div>
   )
 }
