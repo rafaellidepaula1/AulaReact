b2f1b8a048a6473b6d99635c5d5520ad79fe9406
rafaellidepaula1 <rafaellidepaula1@gmail.com>
2026-05-06 14:43:38 -0300

    06/05

--- aula01/src/App.jsx
+++ aula01/src/App.jsx
@@ -1,8 +1,14 @@
-export default function app()
-{
+import "./app.css";
+export default function App()
+ {
   return (
     <div>
       <h1>Aula 02 - estudo de componentes de props</h1>
+
+      <div className="card">
+        <h3>Chamada para o Exemplo1</h3>
+
+      </div>
     </div>
   )
 }
