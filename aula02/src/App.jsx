2341f1278fe16a111ba2e4cf609227164cb6cba9
rafaellidepaula1 <rafaellidepaula1@gmail.com>
2026-05-20 14:50:57 -0300

    20/05

--- aula02/src/App.jsx
+++ aula02/src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import Exemplo1 from "./paginas/Exemplo1"
 import Exemplo2 from "./paginas/Exemplo2";
 import Exercicio1 from "./paginas/Exercicio1";
+import Exercicio2 from "./paginas/Exercicio2";
+import Exercicio3 from "./paginas/Exercicio3";
 export default function App()
 {
   return (
@@ -16,17 +18,28 @@ export default function App()
 
       </div>
       <div className="card">
-       <h3>chmada para o Exmplo2</h3>
+       <h3>chamada para o Exmplo2</h3>
        <Exemplo2 numero1={33} numero2={34}/>
        <Exemplo2 numero1={33} numero2={-345}/>
        <Exemplo2 numero1={33} numero2={6.8}/>
        </div>
 
        <div className="card">
-       <h3>chmada para o Exmplo2</h3>
+       <h3>chamada para o Exmplo2</h3>
        <Exercicio1 Fahrenheint={30} />
+
+      <div className="card">
+       <h3>chamada para o Exercicio2</h3> 
+       <Exercicio2 altura={1.65} peso= {70} />
        
-     
+       </div>
+       <div className="card">
+       <h3>chamada para o Exercicio3</h3> 
+       <Exercicio3 nota1={5} nota2= {6} />
+
+
+        </div> 
+      
       </div>
 
 
--- aula02/src/paginas/Exercicio2.jsx
+++ aula02/src/paginas/Exercicio2.jsx
@@ -1,8 +1,10 @@
-export default function Exercicio2()
+export default function Exercicio2({peso, altura})
 {
+    let imc; 
+    imc = peso /(altura*altura);
     return (
         <div>
-
+             O indice da massa corporal é {imc}
         </div>
     )
 }
